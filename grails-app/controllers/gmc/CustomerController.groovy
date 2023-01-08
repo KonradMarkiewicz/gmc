def52cb4621198fbfdbd8f4d659560e565fd99f1
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2023-01-08 16:46:07 +0100

    managing data with GORM + generated static scaffold for customer domain

--- grails-app/controllers/gmc/CustomerController.groovy
+++ grails-app/controllers/gmc/CustomerController.groovy
@@ -1,9 +1,108 @@
 package gmc
 
-class CustomerController {
-    static scaffold = Customer
+import grails.validation.ValidationException
 
-//    def index() { }
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.http.HttpStatus.NO_CONTENT
+import static org.springframework.http.HttpStatus.OK
+
+class CustomerController {
+
+    CustomerService customerService
+    CalculationsService calculationsService
 
     def checkin() {}
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond customerService.list(params), model:[customerCount: customerService.count()]
+    }
+
+    def show(Long id) {
+        def customer = customerService.get(id)
+        customer = calculationsService.getTotalPoints(customer)
+        respond customer
+    }
+
+    def create() {
+        respond new Customer(params)
+    }
+
+    def save(Customer customer) {
+        if (customer == null) {
+            notFound()
+            return
+        }
+
+        try {
+            customerService.save(customer)
+        } catch (ValidationException e) {
+            respond customer.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'customer.label', default: 'Customer'), customer.id]) as Object
+                redirect customer
+            }
+            '*' { respond customer, [status: CREATED] }
+        }
+    }
+
+    def edit(Long id) {
+        respond customerService.get(id)
+    }
+
+    def update(Customer customer) {
+        if (customer == null) {
+            notFound()
+            return
+        }
+
+        try {
+            customerService.save(customer)
+        } catch (ValidationException e) {
+            respond customer.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'customer.label', default: 'customer'), customer.id])
+                redirect customer
+            }
+            '*'{ respond customer, [status: OK] }
+        }
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        customerService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'customer.label', default: 'Customer'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+
+
 }
--- grails-app/domain/gmc/Customer.groovy
+++ grails-app/domain/gmc/Customer.groovy
@@ -3,13 +3,13 @@ package gmc
 class Customer {
     String firstName
     String lastName
-    Long phone
+    String phone
     String email
     Integer totalPoints
     static hasMany = [awards:Award, orders:OnlineOrder]
 
     static constraints = {
-        phone()
+        phone nullable: true
         firstName nullable: true
         lastName nullable: true
         email nullable: true, email: true
